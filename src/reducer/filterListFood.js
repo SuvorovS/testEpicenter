43cf6007041ba2cc8c2fbbdc6cbb183ed0511ded
SuvorovS <su.vo.rov@mail.ru>
2017-07-23 17:53:50 +0300

    удалил ненужное

--- src/reducer/filterListFood.js
+++
@@ -1,8 +0,0 @@
-export default function filterListFood (state = '', action) {
-        if (action.type === 'SEARCH_FOOD') {
-                return action.payload;
-        }
-        else {
-                return state
-        }
-}
